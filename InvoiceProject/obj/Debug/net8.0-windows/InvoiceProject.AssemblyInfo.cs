d37aca425dc572938e21fae3549dd27865fdec46
BogDuha <ustefan06@gmail.com>
2026-07-22 00:14:16 +0200

    commitaftervc2

--- InvoiceProject/obj/Debug/net8.0-windows/InvoiceProject.AssemblyInfo.cs
+++ InvoiceProject/obj/Debug/net8.0-windows/InvoiceProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686f9dd3c96a1cb29e2d4c4c14a13de05f0792d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10a4de6b1a92772a9f6439e2de4caa5fe823c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
